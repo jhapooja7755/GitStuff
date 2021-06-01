43302591955347001e7b2c98d1fbf900aa838878
jhapooja7755 <jhapooja7755@gmail.com>
2021-06-01 10:29:05 +0530

    first commit

---
+++ src/RestApi.java
@@ -0,0 +1,15 @@
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+
+public class RestApi {
+
+	public static void main(String[] args) {
+    RestAssured.baseURI = "https://google.com";
+    given().body("");
+		
+				
+				
+
+	}
+
+}
